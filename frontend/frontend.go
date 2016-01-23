2a8419c9d8eb457569adf57f6568b6e2eebdf93f
Robert Egorov <robert.egorov@gmail.com>
2016-01-23 03:18:10 +0100

    adds basic backend and frontend objects

---
+++ frontend/frontend.go
@@ -0,0 +1,114 @@
+package frontend
+
+import (
+	"fmt"
+	"github.com/gophergala2016/gobench/backend"
+	"github.com/hydrogen18/stoppableListener"
+	"github.com/labstack/echo"
+	mw "github.com/labstack/echo/middleware"
+	"html/template"
+	"io"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"path"
+	"sync"
+	"syscall"
+)
+
+// Config holds frontend configuration params
+type Config struct {
+	Host           string `json:"host"`
+	Port           int    `json:"port"`
+	TemplateFolder string `json:"templateFolder`
+	AssetFolder    string `json:"assetFolder`
+}
+
+//  Frontend provides single point of access to fronend layer
+type Frontend struct {
+	log    *log.Logger
+	back   *backend.Backend
+	cfg    *Config
+	router *echo.Echo
+}
+
+// New creates Frontend instance, initialises routers
+func New(cfg *Config, l *log.Logger, b *backend.Backend) (*Frontend, error) {
+	f := &Frontend{log: l, back: b, cfg: cfg, router: echo.New()}
+
+	f.router.SetRenderer(f)
+	f.router.HTTP2(false)
+
+	// TODO, write logs into file (probable logwriter can be used)
+	f.router.SetLogOutput(os.Stdout)
+
+	f.router.Use(mw.Logger())
+	f.router.Use(mw.Recover())
+
+	f.router.Static("/css", path.Join(f.cfg.AssetFolder, "/css"))
+	f.router.Static("/img", path.Join(f.cfg.AssetFolder, "/img"))
+	f.router.Static("/js", path.Join(f.cfg.AssetFolder, "/js"))
+	f.router.Favicon(path.Join(f.cfg.AssetFolder, "/img/favicon.ico"))
+
+	// TODO
+	// f.router.SetHTTPErrorHandler(???)
+
+	return f, nil
+}
+
+// Start launches gracefull HTTP listener
+func (f *Frontend) Start() error {
+
+	listenAddr := fmt.Sprintf("%s:%d", f.cfg.Host, f.cfg.Port)
+	originalListener, err := net.Listen("tcp", listenAddr)
+	if err != nil {
+		return err
+	}
+
+	sl, err := stoppableListener.New(originalListener)
+	if err != nil {
+		return err
+	}
+
+	server := http.Server{Handler: f.router}
+
+	stop := make(chan os.Signal)
+	signal.Notify(stop, syscall.SIGINT)
+	var wg sync.WaitGroup
+	go func() {
+		wg.Add(1)
+		defer wg.Done()
+		server.Serve(sl)
+	}()
+
+	f.log.Println("Start serving HTTP requests at ", listenAddr)
+	select {
+	case signal := <-stop:
+		f.log.Println("Got signal: ", signal)
+	}
+	f.log.Println("Stopping listener")
+	sl.Stop()
+	f.log.Println("Waiting on server")
+	wg.Wait()
+
+	return nil
+}
+
+// Render implements Echo's Renderer interface
+func (f *Frontend) Render(w io.Writer, name string, data interface{}) error {
+
+	// TODO: implement templates caching
+	tmpl, err := template.ParseGlob(path.Join(f.cfg.TemplateFolder, name))
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, fmt.Sprintf("Template reading error. Details: %s", name, err.Error()))
+	}
+
+	err = tmpl.ExecuteTemplate(w, name, data)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, fmt.Sprintf("Template rendering error. Details: %s", name, err.Error()))
+	}
+
+	return nil
+}
--- main.go
+++ main.go
@@ -1,6 +1,5 @@
-package main 
+package main
 
 func main() {
-	
-	
+
 }
