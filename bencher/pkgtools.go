b0bd51a120ece8e3e11def28812561928c0b1201
tkola <tmikola@gmail.com>
2016-01-24 13:45:10 +0100

    Implement download dependencies

--- bencher/pkgtools.go
+++ bencher/pkgtools.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"os"
 	"os/exec"
+	
 )
 
 var (
@@ -30,6 +31,15 @@ func downloadPackage(name string) (string, error) {
 }
 
 func downloadPackageDependencies(name string) error {
+
+	cmd := exec.Command("go", "test", "-i", name )
+	err := cmd.Start()
+	if err != nil {
+	    return err
+	}
+	if err = cmd.Wait(); err != nil {
+	    return err
+	}
 
 	return nil
 }
